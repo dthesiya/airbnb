fbf6922914b49f9a98c09ee868ade6a3a719b468
Darshit <darshit.thesiya@sjsu.edu>
2016-11-20 15:38:50 -0800

    first test commit from darshit branch

--- Airbnb_Client/app.js
+++ Airbnb_Client/app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.post('/signin', signin.authenticateUser);
+
 app.get('/signin', isAuthenticated, function(req, res) {
 
   res.redirect('/');
