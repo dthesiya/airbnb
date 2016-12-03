e28038bb00214af3d2cdf0824924e79622cd6baf
Divya Patel <divyakumar.patel@sjsu.edu>
2016-12-02 22:45:15 -0800

    Dynamic Pricing completed

--- Airbnb_server/services/bid.js
+++ Airbnb_server/services/bid.js
@@ -7,6 +7,7 @@ var Trip = require('../model/trip');
 var mongoose = require('mongoose');
 var ssn = require('ssn');
 var DynamicPrice = require('../model/dynamicPrice');
+var Map = require("hashmap");
 var ObjectId = require('mongoose').Types.ObjectId;
 
 exports.updateBasePrice = function (msg, callback) {
@@ -114,8 +115,10 @@ exports.dynamicPriceCron = function (msg,callback) {
 
 
     var makeDate = new Date();
-    makeDate = new Date(makeDate.setMonth(makeDate.getMonth() - 1));
-    Trip.find({createdDate:{$gt:makeDate}},function (err,result) {
+    makeDate = new Date(makeDate.setMonth(makeDate.getMonth() - 1)).getTime();
+    console.log(makeDate);
+
+    Trip.find({createdDate:{$gt:makeDate}}).populate('propertyId').exec(function (err, result) {
 
         if(err){
             console.log(err);
@@ -123,6 +126,87 @@ exports.dynamicPriceCron = function (msg,callback) {
         }
         else{
             console.log(result);
+
+            var map = new Map();
+            var priceMap = new Map();
+
+            for(var i=0;i<result.length;i++){
+
+                
+                var propertyId = result[i].propertyId._id.toString();
+                
+                if(map.has(propertyId)){
+                    map.set(propertyId,map.get(propertyId)+1);
+                }
+                else{
+                    map.set(propertyId,1);
+                    priceMap.set(propertyId,result[i].propertyId.price);
+                }
+            }
+            
+            var propertyIDArray = [];
+            var dynamicpriceArray = [];
+           
+            map.forEach(function(value, key) {
+                if(value>=10 && value<20){
+
+                    propertyIDArray.push(key);
+                    var newPrice = Number(priceMap.get(key))*1.1;
+                    dynamicpriceArray.push(Math.round(newPrice));
+                }
+                else if(value>=20 && value<40){
+
+                    propertyIDArray.push(key);
+                    var newPrice = Number(priceMap.get(key))*1.4;
+                    dynamicpriceArray.push(Math.round(newPrice));
+                }
+                else if(value>=40 && value<50){
+                    propertyIDArray.push(key);
+                    var newPrice = Number(priceMap.get(key))*1.7;
+                    dynamicpriceArray.push(Math.round(newPrice));
+                }
+                else if(value>=50){
+                    propertyIDArray.push(key);
+                    var newPrice = Number(priceMap.get(key))*1.9;
+                    dynamicpriceArray.push(Math.round(newPrice));
+                }
+            });
+
+            var bulk = Property.collection.initializeOrderedBulkOp();
+
+            for(var i=0;i<propertyIDArray.length;i++){
+                
+                console.log("Property id "+ propertyIDArray[i]+ ": "+dynamicpriceArray[i]);
+                Property.update({_id: propertyIDArray[i]}, {
+                    $set: {
+                        price: dynamicpriceArray[i]
+                    }
+                }, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+                    }
+                    if (!result) {
+                        console.log("not updated");
+                        console.log(result);
+                        callback(null, result);
+                    }
+                    if (result) {
+                        console.log("Updated");
+                        console.log(result);
+                        callback(null,result);
+                    }
+                });
+                
+            }
+
+
+
+
+
+
+
+
         }
     });
 };
